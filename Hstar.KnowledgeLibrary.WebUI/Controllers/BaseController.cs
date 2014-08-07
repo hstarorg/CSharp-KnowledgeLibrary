f4bad208c6ef9445049390a914a0e7c7bbf55b64
Hstar <hm910705@163.com>
2014-08-08 00:27:47 +0800

    提交人：胡敏 描述： 1、项目基础上传； 2、仅引入了bootstrap；

---
+++ Hstar.KnowledgeLibrary.WebUI/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace Hstar.KnowledgeLibrary.WebUI.Controllers
+{
+    /// <summary>
+    /// 控制器基类
+    /// </summary>
+    public class BaseController : Controller
+    {
+        public string UserId
+        {
+            get
+            {
+                //Windows认证下的用户名称（当前电脑名/域账户）
+                var userInfo = HttpContext.User.Identity.Name.Split('\\');
+                if (userInfo.Length == 2)
+                {
+                    return userInfo[1];
+                }
+                return string.Empty;
+            }
+        }
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(this.UserId))
+            {
+                filterContext.Result=new HttpUnauthorizedResult("未授权！");
+            }
+            base.OnAuthorization(filterContext);
+        }
+    }
+}
---
+++ Hstar.KnowledgeLibrary.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hstar.KnowledgeLibrary.WebUI.MvcApplication" Language="C#" %>
